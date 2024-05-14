f0973de4a7eb51cdcad6104a4654b48d2e94e0f8
xsjark <xsjark@gmail.com>
2024-05-14 13:32:52 +0900

    changed div id to track-text

--- src/components/TrackText/TrackText.tsx
+++ src/components/TrackText/TrackText.tsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './TrackText.css'
 
 const TrackText: React.FC<{ isVisible: boolean }> = ({ isVisible }) => (
-  <p id="track-text" style={{ display: isVisible ? 'block' : 'none' }}>hello track</p>
+  <div id='track-text' style={{ display: isVisible ? 'block' : 'none' }}>
+      <p>hello track</p>
+    </div>
 );
 
 export default TrackText;
